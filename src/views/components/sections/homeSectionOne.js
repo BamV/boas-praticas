5148bfb09051d79e8f9f9367c2abbe0816ab2e05
Fernando Nazario <fernando_nzario15@hotmail.com>
2021-07-11 18:32:40 -0300

    :tada: initial commit

---
+++ src/views/components/sections/homeSectionOne.js
@@ -0,0 +1,15 @@
+import Logo from '../../../assets/images/logo/logo.png';
+import backGroundOne from '../../../assets/images/background/background.png';
+
+let homeSectionOne = `
+    <section id="section-one" class="section background--one" style="background-image: url(${backGroundOne})" >
+        <div>
+            <div class="container block-section--one">
+                <h1>CLIQUE NO BABY YODA, ELE TE MOSTRARÁ O CAMINHO</h1>
+                <img src="${Logo}" alt="Logo Star Wars" width="300px" height="auto">
+            </div>    
+        </div>
+    </section>
+`;
+
+export default homeSectionOne;
